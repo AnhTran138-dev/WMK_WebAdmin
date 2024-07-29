33d30ad3e2e1085505172a60db1b19882d08e5c6
ATHT138 <anhthtservice@gmail.com>
2024-07-29 10:02:37 +0700

    ui header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,18 +1,48 @@
-import { Moon, Sun } from "lucide-react";
+import { LogOut, Moon, Sun } from "lucide-react";
 import useThemeStore from "../../states/local/theme";
-import { Avatar, AvatarFallback, AvatarImage, Button } from "../ui";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+  Button,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../ui";
+import { useNavigate } from "react-router-dom";
+import { removeItem } from "../../lib";
 
 const Header = () => {
   const { theme, toggleTheme } = useThemeStore();
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div className="flex justify-end gap-4 px-8 py-5 bg-primary">
       <Button variant="outline" onClick={toggleTheme}>
         {theme === "dark" ? <Sun /> : <Moon />}
       </Button>
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button>
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="flex flex-col mr-12 w-fit">
+          <Button
+            variant="ghost"
+            className="gap-5 w-fit"
+            onClick={() => {
+              removeItem("token");
+              navigate("/sign-in");
+            }}
+          >
+            <LogOut /> Logout
+          </Button>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 };
--- src/views/loginpage/login_form.tsx
+++ src/views/loginpage/login_form.tsx
@@ -71,7 +71,7 @@ const LoginForm = () => {
           control={form.control}
           name="password"
           render={({ field }) => (
-            <FormItem className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <FormItem className="block w-full  border-0 py-1.5 shadow-sm ">
               <FormLabel>Password</FormLabel>
               <FormControl>
                 <div className="relative">
