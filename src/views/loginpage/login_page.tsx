6bb0a4f0a4f6c97bce822034264614863c9915af
ATHT138 <anhthtservice@gmail.com>
2024-08-30 03:40:24 +0700

    change style login page

--- src/views/loginpage/login_page.tsx
+++ src/views/loginpage/login_page.tsx
@@ -4,20 +4,26 @@ import assert from "../../asserts";
 
 const LoginPage = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen size-full bg-custom-blue">
-      <img
-        src={assert.logo}
-        alt="WeMealKit"
-        className="rounded-lg shadow-2xl"
-      />
-      <Card className="w-2/6 p-8">
-        <CardHeader>
-          <CardTitle>Welcome to WeMealKit</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <LoginForm />
-        </CardContent>
-      </Card>
+    <div className="flex items-center justify-center min-h-screen p-4 bg-custom-blue">
+      <div className="overflow-hidden shadow-md w-md rounded-xl md:max-w-4xl lg:w-screen">
+        <div className="w-full md:flex">
+          <div className="md:shrink-0">
+            <img
+              src={assert.logo}
+              alt="WeMealKit"
+              className="object-cover size-full md:size-full "
+            />
+          </div>
+          <Card className="flex flex-col justify-center w-full md:max-w-md">
+            <CardHeader>
+              <CardTitle>Welcome to WeMealKit</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <LoginForm />
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 };
